8fc892ad488fff127b508af113fbb118a509f8a2
Annabelle Fait <imannaf8@gmail.com>
2025-01-26 21:18:47 -0500

    Task 3 - Employee Records

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ console.log("Updated Scores", scores); // Log updated array of scores
 let total = scores.reduce((sum, scores) => sum + scores, 0); // Sum up the total of the scores
 let average = total/5; // Find the average of the scores using total divided by the 5 scores
 console.log("Average of all Scores", average); // Log the average of all scores
+
+// Task 3 - Employee Records
+let employee = {
+    name: "employee name", // Name of Employee
+    age: "employee age", // Age of Employee
+    department: "employee department", //Department of Employee
+    isActive: true, // A boolean value representing availability
+}
+console.log("Name:", employee.name); // Accessing property: "employee name"
+console.log("Age:", employee.age); // Accessing property: "employee age"
+console.log("Department:", employee.department); // Accessing propoerty "employee department"
+console.log("isActive:", employee.isActive); // Log the Employee as Active True
+employee.department = "Employee Department 2"; // Update the employee department to employee department 2
+console.log("Updated Department", employee.department); // Log the updated employee department
+employee.position = "CEO"; // Updated employee position to CEO
+console.log("Updated employee position", employee.position) // Log the updated employee position
